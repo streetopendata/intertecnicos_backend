8fde11fc94ec94ef1bc2b754cc79bba1ab58861b
Jesus Contreras <jesuscontreras1996102@gmail.com>
2020-05-11 18:44:18 -0500

    add create registry to daily entrys

--- app/controllers/daily_entrys_controller.rb
+++ app/controllers/daily_entrys_controller.rb
@@ -2,4 +2,25 @@ class DailyEntrysController < ApplicationController
   def new
       @worker = Admin.find(params[:admin_id])
   end
+
+  def create
+    @daily_entry = current_admin.daily_entrys.new(daily_entrys_params)
+    if @daily_entry.save
+      redirect_to  admin_workers_url(current_admin.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def daily_entrys_params
+    params.require(:daily_entrys)
+          .permit(
+                  :temperature_ini,
+                  :temperature_fin,
+                  :respiratory_symptom,
+                  :contact_with_infected,
+                  :start_day,
+                  :final_day)
+  end
 end
