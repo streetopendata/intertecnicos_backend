cd64cae684794c4a93b7b34cac08b868123a7ae9
Jesus Contreras <jesuscontreras1996102@gmail.com>
2020-05-14 16:39:03 -0500

    add more puma configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.0"
 
-set :puma_conf, "/var/www/intertecnicos/shared/config/puma.rb"
+
 set :application, 'intertecnicos'
 set :repo_url, 'git@github.com:streetopendata/intertecnicos_backend.git'
 set :deploy_to, '/home/ubuntu/intertecnicos'
@@ -43,3 +43,32 @@ set :linked_files, %w{config/master.key}
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+
+set :puma_conf, "/var/www/intertecnicos/shared/config/puma.rb"
+
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+
+set :puma_role, :app
+
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+
+set :puma_threads, [0, 8]
+
+set :puma_workers, 0
+
+set :puma_worker_timeout, nil
+
+set :puma_init_active_record, true
+
+set :puma_preload_app, false
