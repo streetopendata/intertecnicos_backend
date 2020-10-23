0cdd7a33ab2e842484d1ea3f108cffbb3d62ece0
Jesus Contreras <jesuscontreras1996102@gmail.com>
2020-10-23 11:41:19 -0500

    Add and configure Lines blog engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 
 Rails.application.routes.draw do
+  mount Lines::Engine => "/blog"
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
